b63f3601da3f3e1ac6f7e33987c1001a033525c7
Brooke M. Angel <angelb@prclab1.erau.edu>
2026-03-03 13:37:07 -0700

    Added header file and maincode file

---
+++ asciiart.h
@@ -0,0 +1,7 @@
+/********************************
+This is our file for ascii art.
+********************************/
+
+#include <stdio.h>
+#include <string.h>
+
---
+++ maincode.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <asciiart.h>
+#include <string.h>
+
+int main() {
+
+	printf("Welcome to the chatbot!\n");
+	printf("This game will allow you to build relationships with a variety of people and learn more about them.\n");
+
+return 0; }
