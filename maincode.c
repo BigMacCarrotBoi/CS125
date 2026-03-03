3a79d08f85f87b89cde19133075fdc82a6d6ab69
Brooke M. Angel <angelb@prclab1.erau.edu>
2026-03-03 13:47:32 -0700

    edited maincode to include starting dialogue

--- maincode.c
+++ maincode.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
-#include <asciiart.h>
+#include "asciiart.h"
 #include <string.h>
+#include <stdlib.h>
+
+/* This function clears the terminal screen for the user. */
+void clearscreen() {
+
+	system("clear");
+
+}
+
 
 int main() {
 
+	clearscreen();
 	printf("Welcome to the chatbot!\n");
 	printf("This game will allow you to build relationships with a variety of people and learn more about them.\n");
 
